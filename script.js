e37b7188b0eb231a0b5b8dda7b6188f559681a3e
Sara <slushpilesmile@gmail.com>
2020-10-11 01:24:44 -0400

    Formatted second half of page (actual conversion boxes) and added JavaScript functions to calculate Smoots to feet and vice versa

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-.converter {
+function convertToSmoots() {
 
-    background: #F4D3CC;
-    
+    var feetInput = document.getElementById("entry1").value;
+
+    var intermediate1 = parseInt(feetInput);
+
+    var intermediate2 = feetInput / 12;
+
+    var convertedSmoots = intermediate * 67;
+
+    document.getElementById("convertedSmoots").innerHTML = convertedSmoots;
+
+}
+
+function convertToFeet() {
+
+
 
 }
